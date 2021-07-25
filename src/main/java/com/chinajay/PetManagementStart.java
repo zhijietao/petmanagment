7fe4f811f90c5374ee26aa1048b4cd03fe83a284
taozj <13131713@qq.com>
2021-07-25 15:34:52 +0800

    IDEA 整合 GITHUB 测试2

--- src/main/java/com/chinajay/PetManagementStart.java
+++ src/main/java/com/chinajay/PetManagementStart.java
@@ -3,5 +3,6 @@ package com.chinajay;
 public class PetManagementStart {
     public static void main(String[] args) {
         System.out.println("Pet Management System Released.");
+        System.out.println("Wow, this Project had been shared on git");
     }
 }
